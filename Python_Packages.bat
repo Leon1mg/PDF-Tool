be2f3dde7bc66d155a8580c8c9bf387163bdb88d
Leon <106867796+Leon1mg@users.noreply.github.com>
2024-12-03 09:51:21 +0100

    Update Python_Packages.bat

--- Python_Packages.bat
+++ Python_Packages.bat
@@ -18,6 +18,7 @@ pip install tk
 pip install pillow
 pip install pymupdf
 pip install pywin32
+pip install pypdf2
 
 echo.
 echo Überprüfung der Installation:
@@ -25,6 +26,7 @@ pip show tk >nul 2>&1 && echo - tk installiert || echo - tk NICHT installiert
 pip show pillow >nul 2>&1 && echo - Pillow installiert || echo - Pillow NICHT installiert
 pip show pymupdf >nul 2>&1 && echo - PyMuPDF installiert || echo - PyMuPDF NICHT installiert
 pip show pywin32 >nul 2>&1 && echo - pywin32 installiert || echo - pywin32 NICHT installiert
+pip show pypdf2 >nul 2>&1 && echo - pypdf2 installiert || echo - pypdf2 NICHT installiert
 
 echo.
 echo Installation abgeschlossen.
